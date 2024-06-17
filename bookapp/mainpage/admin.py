30359a849f5ef15944932dc5eefc8e3b20a4079e
Paulina Medel <paulinamedel04@gmail.com>
2024-06-17 20:10:42 +0200

    add models

--- bookapp/mainpage/admin.py
+++ bookapp/mainpage/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book, Review, Category
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- bookapp/mainpage/models.py
+++ bookapp/mainpage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=30, blank=True, null=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50, blank=True, null=True)
+    author = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    cover = models.FileField(upload_to='uploads/covers', blank=True, null=True)
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    content = models.TextField(max_length=200, blank=True, null=True)
+    raiting = models.IntegerField(default=0, blank=True, null=True)
